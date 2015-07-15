a9e96752e32488549156f8a7d3b189e6020aed6e
Velsu <riftmage50@gmail.com>
2015-07-15 17:18:04 +0200

    Fix product model, add validations, add average rating function

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,16 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates :price, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/}
+  validates :title, presence: true
+  validates :description, presence: true
+
+  def average_rating
+  	rates = self.reviews.inject(0) do |sum, review|
+  		sum += review.rating
+  	end
+  	size = self.reviews.size
+		return rates.to_f / size if size > 0
+  end
 end
