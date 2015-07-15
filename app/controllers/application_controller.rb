dde0fd2091f850209794ca5c241985f339bb1397
Velsu <riftmage50@gmail.com>
2015-07-15 12:16:32 +0200

    Add firstname and lastname to users, fix devise routes, tweak devise forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,11 @@ class ApplicationController < ActionController::Base
   end
 
   protect_from_forgery with: :exception
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :firstname
+    devise_parameter_sanitizer.for(:account_update) << :firstname
+    devise_parameter_sanitizer.for(:sign_up) << :lastname
+    devise_parameter_sanitizer.for(:account_update) << :lastname
+  end
 end
